24e5a48abbcb5c7cff71ca7e93ec0eafcc3fdf59
Empoy Noble <empoynoble@yahoo.com>
2019-10-14 14:05:13 +0800

    Update 10-14-19

--- script.js
+++ script.js
@@ -2,34 +2,35 @@
 
 var inp = document.getElementById('userinput');
 var ent = document.getElementById('enter');
-var mainlist = document.querySelector('ul');
+var mainlist = document.getElementById('todolist');
 var listItems = document.getElementsByTagName('li');
 
-//add item function
 
+//add item function with delete button
 function createItem(){
 	var crtli = document.createElement('li');
-		crtli.innerHTML = inp.value;
-		mainlist.appendChild(crtli);
-		inp.value = "";
-}
+	crtli.innerHTML = inp.value;
+	mainlist.appendChild(crtli);
+	inp.value = "";
 
+	var delBtn = document.createElement('button');
+	delBtn.innerHTML = "&#215;";
+	crtli.appendChild(delBtn);
+	delBtn.onclick = delItem;
+	delBtn.className = "btn btn-secondary deleteBtn btn-sm";
+}
 
 ent.addEventListener("click", function(){
 	// console.log("click is working");
-
 	if (inp.value.length > 0){
 		createItem();
-		createDelBtn();
 	}
 })
 
 inp.addEventListener("keypress", function(){
 	// console.log("click is working");
-
 	if (inp.value.length > 0 && event.which == 13){
 		createItem();
-		createDelBtn();
 	}
 })
 
@@ -38,20 +39,28 @@ mainlist.onclick = function(event){
 	event.target.classList.toggle('done');
 }
 
-// delete button
-
+// delete button on the default items
 function createDelBtn(){
 	var delBtn = document.createElement('button');
-	delBtn.innerHTML = "Delete";
+	delBtn.innerHTML = "&#215;";
 	listItems[i].appendChild(delBtn);
 	delBtn.onclick = delItem;
+	delBtn.className = "btn btn-secondary deleteBtn btn-sm";
 }
-
 
 for (i = 0; i < listItems.length; i++){
 	createDelBtn();
 }
 
+//delete per item
 function delItem(evt){
 	evt.target.parentNode.remove();
+}
+
+//delete all
+function delAll(){
+	// mainlist.remove();
+	while (mainlist.hasChildNodes()) {
+    mainlist.removeChild(mainlist.firstChild);
+  }
 }
